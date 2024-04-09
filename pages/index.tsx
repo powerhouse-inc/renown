5032669813a0687afdad8b4a7055d52301827e3b
acaldas <acaldas@powerhouse.inc>
2024-04-09 21:03:35 +0100

    feat: added landing page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
 import Connect from "../components/connect-flow";
 import { useEffect, useState } from "react";
+import RenownLight from "../assets/images/Renown-light.svg";
+import PhIcons from "../assets/images/ph-icons.svg";
+import LandingGradient from "../assets/images/landing-gradient.jpg";
+import Noise from "../assets/images/noise.png";
 
 const Home: NextPage = () => {
     const router = useRouter();
@@ -16,18 +21,44 @@ const Home: NextPage = () => {
     }, []);
 
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Renown</title>
-                <meta content="Created by Powerhouse" name="description" />
-                <link href="/favicon.ico" rel="icon" />
-            </Head>
+        <div className="relative">
+            <div className="absolute w-screen h-screen overflow-hidden z-0 fill-[#404040]">
+                <Image
+                    src={LandingGradient}
+                    alt="background"
+                    className="absolute w-[101vw] max-w-none h-screen z-0 object-cover"
+                />
+                <Image
+                    src={Noise}
+                    alt="noise"
+                    className="absolute w-[101vw] max-w-none h-screen z-0 object-cover"
+                />
+                <Image
+                    src={RenownLight}
+                    alt="Renown"
+                    width={154}
+                    height={48}
+                    className="absolute left-8 top-3"
+                />
+                <Image
+                    src={PhIcons}
+                    alt="Renown"
+                    className="absolute left-0 bottom-0 pointer-events-none"
+                />
+            </div>
+            <div className={styles.container}>
+                <Head>
+                    <title>Renown</title>
+                    <meta content="Created by Powerhouse" name="description" />
+                    <link href="/favicon.ico" rel="icon" />
+                </Head>
 
-            <main className={styles.main}>
-                {connectId && isClient && (
-                    <Connect connectId={connectId} deeplink={deeplink} />
-                )}
-            </main>
+                <main className={styles.main}>
+                    {connectId && isClient && (
+                        <Connect connectId={connectId} deeplink={deeplink} />
+                    )}
+                </main>
+            </div>
         </div>
     );
 };
