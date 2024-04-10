9ebe73b0c29f99c7c9060b5aa05c6aef66d8c4b4
acaldas <acaldas@powerhouse.inc>
2024-04-10 12:36:49 +0100

    feat: added cors headers

--- pages/api/[utils].ts
+++ pages/api/[utils].ts
@@ -1,8 +1,20 @@
 import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 
+const ORIGIN_URL = process.env.ORIGIN_URL || "*";
+
 export const allowCors =
     (fn: NextApiHandler) =>
     async (req: NextApiRequest, res: NextApiResponse) => {
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", ORIGIN_URL);
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET,DELETE,PATCH,POST,PUT"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+        );
         if (req.method === "OPTIONS") {
             res.status(200).end();
             return;
